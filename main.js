ca5d8c9a84a31804dd9dc1d31d35f6a61084b816
liurongqing <phpgege@gmail.com>
2020-08-16 10:45:29 +0800

    添加初版

--- main.js
+++ main.js
@@ -1,30 +1,13 @@
-class MyComponent {
+import { createElement, render, Component, render } from './toy-react'
 
+class MyComponent extends Component{
+    render(){
+        return <div>xxx</div>
+    }
 }
 
-window.a = <MyComponent id="a" class="c">
-    <div>123</div>
-    <div></div>
-</MyComponent>
+render(<MyComponent id="a" class="c">
+<div>123</div>
+<div></div>
+</MyComponent>, document.querySelector('#root'))
 
-
-function createElement(type, attributes, ...children) {
-    let e
-    if (typeof type === 'string') {
-        e = document.createElement(type)
-    } else {
-        e = new type
-    }
-
-    for (let p in attributes) {
-        e.setAttribute(p, attributes[p])
-    }
-    console.log('children', children)
-    for (let child of children) {
-        if (typeof child === 'string') {
-            child = document.createTextNode(child)
-        }
-        e.appendChild(child)
-    }
-    return e
-}
